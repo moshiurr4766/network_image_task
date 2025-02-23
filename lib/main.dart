5a050609cbf82f1abac58048916f77f76b44ca56
moshiurr4766 <rahman15-4766@diu.edu.bd>
2025-02-23 12:21:07 +0600

    work_with_drawer

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,60 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+      child: ListView(
+        children: [
+          SizedBox(
+            height: 100,
+            child: DrawerHeader(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.tiktok),
+                  SizedBox(width: 150,),
+                  TextButton(
+                    onPressed: (){}, 
+                    child: Icon(Icons.close)),
+                ],
+
+              ),
+            ),
+          ),
+
+          ListTile(
+            focusColor: Colors.grey[500],
+            title: Text("Home"),
+            leading: Icon(Icons.home),
+            onTap: (){},
+          ),
+          ListTile(
+            focusColor: Colors.grey[600],
+            title: Text("Telegram"),
+            leading: Icon(Icons.near_me),
+            onTap: (){},
+          ),
+          ListTile(
+            focusColor: Colors.grey[500],
+            title: Text("What's Up"),
+            leading: Icon(Icons.whatshot),
+            onTap: (){},
+          ),
+          ListTile(
+            focusColor: Colors.grey[600],
+            title: Text("Facebook"),
+            leading: Icon(Icons.facebook),
+            onTap: (){},
+          ),
+          ListTile(
+            focusColor: Colors.grey[500],
+            title: Text("About"),
+            leading: Icon(Icons.contrast),
+            onTap: (){},
+          ),
+        ],
+      ),
+      ),
+
       appBar: AppBar(
         backgroundColor: Colors.grey[700],
         title: Text("AI META",),
