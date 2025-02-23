82b5d82c59566af9be8eeda6494546a3bbf39cfe
moshiurr4766 <rahman15-4766@diu.edu.bd>
2025-02-23 13:07:21 +0600

    final_bottom_nav_update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/appBody.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -17,7 +18,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    
+    body: Appbody(),
     drawer: Drawer(
       child: ListView(
         children: [
@@ -76,61 +77,6 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.grey[700],
         title: Text("AI META",),
         centerTitle: true,
-      ),
-      
-    body: Container(
-        padding: EdgeInsets.all(0.0),
-        child: Column(
-            children: [
-            Expanded(
-              child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                image: NetworkImage(
-                  'https://images.unsplash.com/photo-1673255745677-e36f618550d1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YWl8ZW58MHx8MHx8fDA%3D',
-                ),
-                fit: BoxFit.cover,
-                ),
-              ),
-              child: Column(
-                children: [
-                  SizedBox(height: 600,),
-                  Center(
-                    child: TextButton(
-                      child: Text(
-                        'WELCOME TO AI META',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),
-                ],
-              ),
-              ),
-            ),
-          ],
-        ),
-      ),
-
-    bottomNavigationBar: BottomNavigationBar(
-      items: [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: "Home",
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.usb_rounded),
-          label: "Profile",
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.mail_rounded),
-          label: "Mail",
-        ),
-      ]
       ),
     
     );
